32f5bd0ae80e14da7e5ccbc960659fe95e0ed7a9
Iryna Smirnova <Iryna_Smirnova@epam.com>
2015-07-29 20:16:48 +0300

    gulpfile finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,44 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var jasmine = require('gulp-jasmine');
- 
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var LessPluginCleanCSS = require('less-plugin-clean-css'),
+    cleancss = new LessPluginCleanCSS({ advanced: true });
+
+gulp.task('jscs', function () {
+    return gulp.src('all.js')
+        .pipe(jscs())
+});
+
+gulp.task('jshint', function () {
+    return gulp.src('all.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
+});
+
 gulp.task('scripts', function() {
-  return gulp.src(['./bower_components/bootstrap/js/*.js'])
-    .pipe(concat('dist.js'))
-    .pipe(gulp.dest('./dist/'));
+  return gulp.src(['./bower_components/bootstrap/js/*.js', 'all.js'])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('tests', function () {
     return gulp.src('spec/test.js')
         .pipe(jasmine());
 });
-gulp.task('default', ['scripts', 'tests']);
+
+gulp.task('css', function () {
+    return gulp.src(['./bower_components/bootstrap/less/bootstrap.less','all.less'])
+        .pipe(less({
+            plugins: [cleancss]
+        }))
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', ['jscs','jshint','scripts', 'css','tests']);
--- spec/test.js
+++ spec/test.js
@@ -1,6 +1,9 @@
 var sum = require("../all.js");
 describe("MyFunction", function () {
-    it("should work", function () {
+    it("should be defined", function () {
+        expect(sum).toBeDefined();
+    });
+    it("should be able to sum digits", function () {
         expect(sum(5,6)).toEqual(11);
     });
 });
